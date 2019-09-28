2d8f7a6be85d904268461cf2f1bcdc06320856aa
hezzy <hezzbyte@gmail.com>
2019-09-28 11:01:57 +0100

    dds

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -123,3 +123,27 @@ cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT
 function alertme(){
 	app.dialog.alert("Alert Works");
 }
+
+
+var scanApp = {   
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady);
+    },    onDeviceReady: function () {
+        console.log('Received Device Ready Event');
+        Log.initialize(app.displayLogLine);
+    },
+    scan: function () {
+        cordova.plugins.barcodeScanner.scan(
+                function (result) {
+                    alert("Barcode/QR code data\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
+                },
+                function (error) {
+                    alert("Scanning failed: " + error);
+                }
+        );
+    },
+};
+
