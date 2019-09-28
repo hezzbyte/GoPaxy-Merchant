e6189a962aeb360d976a352c2581ba3530e78890
hezzy <hezzbyte@gmail.com>
2019-09-28 09:22:42 +0100

    Initial commit

---
+++ platforms/android/app/src/main/assets/www/js/app.js
@@ -0,0 +1,139 @@
+// Dom7
+var $$ = Dom7;
+/*
+ $$(document).on('deviceready',function(){ 
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("f9c6d067-9970-4ff0-8941-63e3b85b568f")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+ })*/
+
+
+// Framework7 App main instance
+var app  = new Framework7({
+
+  root: '#app', // App root element
+  id: 'com.gopaxy.app', // App bundle ID
+  name: 'GoPaxy', // App name
+  theme: 'auto', // Automatic theme detection
+  // App root data
+  data: function () {
+    return {
+      user: {
+        firstName: '',
+        lastName: '',
+      },
+    };
+  },
+  // App root methods
+  methods: {
+    helloWorld: function () {
+      app.dialog.alert('Hello World!');
+    },
+  },
+	view: {
+		pushState :true,
+		stackPages: true,            
+	},
+  // App routes
+  routes: routes,
+});
+
+// Init/Create main view
+var mainView = app.views.create('.view-main', {
+  url: '/'
+});
+
+//Set for URL
+var formURL = 'https://www.gopaxy.com/app1';
+
+document.addEventListener('backbutton', backPressed, false);
+function backPressed(){
+        window.history.back();
+}
+
+function nformat(x){
+  var parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return parts.join(".");
+}
+
+
+function trans(){
+	var reqst = 'trans';
+	var userID = localStorage.appUserID;
+	app.preloader.show();
+	app.request.get(formURL, {req: reqst, user: userID}, function (data) {
+	$$('.trans').html(data);  
+	app.preloader.hide();
+	});	
+}
+
+//Function - switch a view/tab
+function switchv(tohide, toshow){
+	$$(tohide).hide();
+	$$(toshow).show();
+}
+
+//Function - show updated wallet value
+function updatewall(){
+	var reqst = 'updatewall';
+	var userID = localStorage.appUserID;
+	app.request.get(formURL, {req: reqst, user: userID}, function (data) {
+		    localStorage.appWallet = data;
+			$$('.appWallet').text('₦' + nformat(localStorage.appWallet));		
+	});	
+}
+
+//Load user details
+function loadContent(){
+	$$('.appFullName').text(localStorage.appFullName);
+	$$('.appWallet').text('₦' + nformat(localStorage.appWallet));
+	$$('.appUserName').text(localStorage.appUserName);
+	//$$('.appUserEmail').text(localStorage.appUserEmail);
+	//$$('.appUserPhone').text(localStorage.appUserPhone);
+}
+
+
+$$(document).on('page:init', function (e) {
+	loadContent();
+});
+
+$$(document).on('page:init', '.page[data-name="my-profile"]', function (e) {
+
+  });
+
+$$(document).on('page:init', '.page[data-name="wallet"]', function (e) {
+	trans();
+  });
+
+function scanbarcode(){
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
+}
---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,59 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/src/browser/SplashScreenProxy.js",
+        "id": "cordova-plugin-splashscreen.SplashScreenProxy",
+        "pluginId": "cordova-plugin-splashscreen",
+        "runs": true
+    },
+    {
+        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+        "id": "onesignal-cordova-plugin.OneSignal",
+        "pluginId": "onesignal-cordova-plugin",
+        "clobbers": [
+            "OneSignal"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/src/browser/BarcodeScannerProxy.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScannerProxy",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-statusbar": "1.0.1",
+    "cordova-plugin-splashscreen": "5.0.3",
+    "onesignal-cordova-plugin": "2.6.0",
+    "phonegap-plugin-barcodescanner": "8.1.0"
+}
+// BOTTOM OF METADATA
+});
