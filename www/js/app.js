02bfb7a6857870e2afa2910c2d8995208587235f
hezzy <hezzbyte@gmail.com>
2019-09-28 11:53:43 +0100

    cc

--- www/js/app.js
+++ www/js/app.js
@@ -111,39 +111,33 @@ $$(document).on('page:init', '.page[data-name="wallet"]', function (e) {
 	trans();
   });
 
-function scanbarcode(){  
-cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-        );
+function scanbarcode(){
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : false, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          //formats : "QR_CODE,UPC_A,UPC_E, EAN_8", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+   );
 }
 
 function alertme(){
 	app.dialog.alert("Alert Works");
 }
-
-
-var scanApp = {   
-    // Application Constructor
-    initialize: function () {
-        this.bindEvents();
-    },    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady);
-    },    onDeviceReady: function () {
-        console.log('Received Device Ready Event');
-        Log.initialize(app.displayLogLine);
-    },
-    scan: function () {
-        cordova.plugins.barcodeScanner.scan(
-                function (result) {
-                    alert("Barcode/QR code data\n" + "Result: " + result.text + "\n" + "Format: " + result.format + "\n" + "Cancelled: " + result.cancelled);
-                },
-                function (error) {
-                    alert("Scanning failed: " + error);
-                }
-        );
-    },
-};
-
